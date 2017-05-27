da899032e408da38c857500f6e528e5b0878fc74
Oskar Pol <oskarbank@gmail.com>
2017-05-27 17:29:01 +0200

    form

--- src/main/java/pl/oskarpolak/newspring/controllers/MainController.java
+++ src/main/java/pl/oskarpolak/newspring/controllers/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import pl.oskarpolak.newspring.models.SimpleBean;
 
 import java.time.LocalDateTime;
@@ -27,12 +29,18 @@ public class MainController {
         System.out.println("Time now: " + now.toString());
 
         if(now.toLocalTime().getHour() >= 16){
-            model.addAttribute("someString", "Czas iść na piwko!");
+            model.addAttribute("someString", "a");
         }else{
-            model.addAttribute("someString", "Muszę posiedzieć jeszcze z tym Oskarem...");
+            model.addAttribute("someString", "b");
         }
 
         return "index";
+    }
+
+    @RequestMapping(value = "/data", method = RequestMethod.POST)
+    @ResponseBody
+    public String data(@RequestParam(value = "name", required = false) String nameFromForm){
+        return "Twoje imię to: " + nameFromForm;
     }
 
 
