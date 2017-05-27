2bf319f654f0eab1703ac5e6e521b1208fe07878
Oskar Pol <oskarbank@gmail.com>
2017-05-27 16:45:30 +0200

    asd

--- src/main/java/pl/oskarpolak/newspring/controllers/MainController.java
+++ src/main/java/pl/oskarpolak/newspring/controllers/MainController.java
@@ -7,16 +7,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import pl.oskarpolak.newspring.models.SimpleBean;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 @Controller
 public class MainController {
 
     @Autowired
     SimpleBean simpleBean;
 
+   // Logger logger = Logger.getLogger(MainController.class);
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String main(Model model) {
-        model.addAttribute("someString", "Pochodzę z controllera!");
+
+        ZonedDateTime now = LocalDateTime.now().atZone(ZoneId.of("Europe/Paris"));
+
+        System.out.println("Time now: " + now.toString());
+
+        if(now.toLocalTime().getHour() >= 16){
+            model.addAttribute("someString", "Czas iść na piwko!");
+        }else{
+            model.addAttribute("someString", "Muszę posiedzieć jeszcze z tym Oskarem...");
+        }
+
         return "index";
     }
 
